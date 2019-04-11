524a8c01f3c9b890dbdbe173c124c5e8797d9c89
Joose Helle <joose.helle@helsinki.fi>
2019-04-11 17:30:01 +0300

    Feat: origin & target locations can be selected

--- src/reducers/originTargetReducer.js
+++ src/reducers/originTargetReducer.js
@@ -1,5 +1,6 @@
 import { turf } from '../utils/index'
 import { initialOriginTargetFeatures } from './../constants'
+import { closePopup } from './mapPopupReducer'
 
 const initialOriginTarget = {
   originTargetFC: turf.asFeatureCollection(initialOriginTargetFeatures),
@@ -8,28 +9,53 @@ const initialOriginTarget = {
 const pathsReducer = (store = initialOriginTarget, action) => {
 
   switch (action.type) {
-    case 'SET_ORIGIN_TARGET':
-      return {
-        ...store,
-        originTargetFC: turf.asFeatureCollection([action.originTarget])
-      }
 
     case 'RESET_ORIGIN_TARGET':
       return initialOriginTarget
+
+    case 'SET_ORIGIN': {
+      const originTargetFC = updateOriginToFC(store.originTargetFC, action.lngLat)
+      return { ...store, originTargetFC }
+    }
+
+    case 'SET_TARGET': {
+      const originTargetFC = updateTargetToFC(store.originTargetFC, action.lngLat)
+      return { ...store, originTargetFC }
+    }
 
     default:
       return store
   }
 }
 
-export const setOrigin = (originlngLat) => {
-  console.log('SET_ORIGIN', originlngLat)
-  return ({ type: 'SET_ORIGIN', originlngLat })
+export const setOrigin = (lngLat) => {
+  return async (dispatch) => {
+    dispatch({ type: 'SET_ORIGIN', lngLat })
+    dispatch(closePopup())
+  }
 }
 
-export const setTarget = (targetlngLat) => {
-  console.log('SET_TARGET', targetlngLat)
-  return ({ type: 'SET_TARGET', targetlngLat })
+export const setTarget = (lngLat) => {
+  return async (dispatch) => {
+    dispatch({ type: 'SET_TARGET', lngLat })
+    dispatch(closePopup())
+  }
+}
+
+const updateOriginToFC = (FC, lngLat) => {
+  const features = FC.features
+  const target = features.filter(feat => feat.properties.type === 'target')
+  const coords = [lngLat.lng, lngLat.lat]
+  const origin = [turf.asPoint(coords, { type: 'origin' })]
+  return turf.asFeatureCollection(origin.concat(target))
+}
+
+const updateTargetToFC = (FC, lngLat) => {
+  const features = FC.features
+  const origin = features.filter(feat => feat.properties.type === 'origin')
+  const coords = [lngLat.lng, lngLat.lat]
+  const target = [turf.asPoint(coords, { type: 'target' })]
+  return turf.asFeatureCollection(target.concat(origin))
 }
 
 export default pathsReducer
