f2c3c3db82be9f3f407db6d2a2d254974dc95adf
Joose Helle <joose.helle@helsinki.fi>
2019-04-17 09:52:08 +0300

    refactor

--- src/components/PathInfo.js
+++ src/components/PathInfo.js
@@ -22,19 +22,19 @@ const StyledPathStats = styled.div`
     background-color: #0e2702c2;
   `}
 `
-  const StyledPathName = styled.div`
+const PathName = styled.div`
   margin: 0 1px 0 0px;
   font-size: 15px;
   padding: 1px 4px;
   height: fit-content;
 `
-const StyledLenDiff = styled.div`
+const LenDiff = styled.div`
   margin: 0 1px 0 0px;
   padding: 1px 4px;
   font-size: 12px;
   height: fit-content;
 `
-const StyledDb = styled.div`
+const Db = styled.div`
   border-radius: 4px;
   background-color: #c59300;
   padding: 2px 5px;
@@ -43,19 +43,19 @@ const StyledDb = styled.div`
   width: fit-content;
   background-color: #000000db;
 `
-const StyledDbDiff = styled(StyledDb)`
+const LenDiffBox = styled(Db)`
   background-color: #af2f2f;
   ${props => props.dB < 0 && css`
     background-color: green;
   `}
   ${props => props.dB > 0 && css`
-    background-color: #af2f2f;
+    background-color: transparent;
   `}
   ${props => props.blank === true && css`
     background-color: transparent;
   `}
 `
-const FlexColsName = styled.div`
+const PathInfoFlex = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
@@ -67,22 +67,18 @@ const FlexCols = styled.div`
   justify-content: space-evenly;
   width: 118px;
 `
-const StyledThDb = styled.div`
+const DbLenFlex = styled.div`
   display: flex;
   margin: 0px;
 `
 
-const ThDB = ({ dB, value, box }) => {
+const DbLenPair = ({ dB, value, box }) => {
   const blank = value === 0 || box === false
   return (
-    <StyledThDb>
-      <StyledDb>
-        {dB} dB
-      </StyledDb>
-      <StyledDbDiff dB={value} blank={blank}>
-        {value} m
-      </StyledDbDiff>
-    </StyledThDb>
+    <DbLenFlex>
+      <Db> {dB} dB</Db>
+      <LenDiffBox dB={value} blank={blank}>{value} m</LenDiffBox>
+    </DbLenFlex>
   )
 }
 
@@ -90,22 +86,22 @@ const ShortPathStats = ({ s_paths }) => {
   const sPath = s_paths[0]
   return (
     <StyledPathStats>
-      <FlexColsName>
-          <StyledPathName>{utils.getKmFromM(sPath.properties.length)} km </StyledPathName>
-          <StyledLenDiff>Shortest</StyledLenDiff>
-        </FlexColsName>
-          <FlexCols>
-            <ThDB box={false} dB={50} value={utils.formatDiffM(sPath.properties.noises[50], false)} m />
-            <ThDB box={false} dB={55} value={utils.formatDiffM(sPath.properties.noises[55], false)} m />
-          </FlexCols>
-          <FlexCols>
-            <ThDB box={false} dB={60} value={utils.formatDiffM(sPath.properties.noises[60], false)} m />
-            <ThDB box={false} dB={65} value={utils.formatDiffM(sPath.properties.noises[65], false)} m />
-          </FlexCols>
-          <FlexCols>
-            <ThDB box={false} dB={70} value={utils.formatDiffM(sPath.properties.noises[70], false)} m />
-            <ThDB box={false} dB={75} value={utils.formatDiffM(sPath.properties.noises[75], false)} m />
-          </FlexCols>
+      <PathInfoFlex>
+        <PathName>{utils.getKmFromM(sPath.properties.length)} km </PathName>
+        <LenDiff>Shortest</LenDiff>
+      </PathInfoFlex>
+      <FlexCols>
+        <DbLenPair box={false} dB={50} value={utils.formatDiffM(sPath.properties.noises[50], false)} m />
+        <DbLenPair box={false} dB={55} value={utils.formatDiffM(sPath.properties.noises[55], false)} m />
+      </FlexCols>
+      <FlexCols>
+        <DbLenPair box={false} dB={60} value={utils.formatDiffM(sPath.properties.noises[60], false)} m />
+        <DbLenPair box={false} dB={65} value={utils.formatDiffM(sPath.properties.noises[65], false)} m />
+      </FlexCols>
+      <FlexCols>
+        <DbLenPair box={false} dB={70} value={utils.formatDiffM(sPath.properties.noises[70], false)} m />
+        <DbLenPair box={false} dB={75} value={utils.formatDiffM(sPath.properties.noises[75], false)} m />
+      </FlexCols>
     </StyledPathStats>
   )
 }
@@ -115,21 +111,21 @@ const QuietPathStats = ({ q_paths }) => {
     <div>
       {q_paths.map(path => (
         <StyledPathStats green key={path.properties.length}>
-        <FlexColsName>
-          <StyledPathName>{utils.getKmFromM(path.properties.length)} km </StyledPathName>
-          <StyledLenDiff>{utils.formatDiffM(path.properties.diff_len, true)} m{' '}</StyledLenDiff>
-        </FlexColsName>
+          <PathInfoFlex>
+            <PathName>{utils.getKmFromM(path.properties.length)} km </PathName>
+            <LenDiff>{utils.formatDiffM(path.properties.diff_len, true)} m{' '}</LenDiff>
+          </PathInfoFlex>
           <FlexCols>
-            <ThDB dB={50} value={utils.formatDiffM(path.properties.noises_diff[50], true)} m />
-            <ThDB dB={55} value={utils.formatDiffM(path.properties.noises_diff[55], true)} m />
+            <DbLenPair dB={50} value={utils.formatDiffM(path.properties.noises_diff[50], true)} m />
+            <DbLenPair dB={55} value={utils.formatDiffM(path.properties.noises_diff[55], true)} m />
           </FlexCols>
           <FlexCols>
-            <ThDB dB={60} value={utils.formatDiffM(path.properties.noises_diff[60], true)} m />
-            <ThDB dB={65} value={utils.formatDiffM(path.properties.noises_diff[65], true)} m />
+            <DbLenPair dB={60} value={utils.formatDiffM(path.properties.noises_diff[60], true)} m />
+            <DbLenPair dB={65} value={utils.formatDiffM(path.properties.noises_diff[65], true)} m />
           </FlexCols>
           <FlexCols>
-            <ThDB dB={70} value={utils.formatDiffM(path.properties.noises_diff[70], true)} m />
-            <ThDB dB={75} value={utils.formatDiffM(path.properties.noises_diff[75], true)} m />
+            <DbLenPair dB={70} value={utils.formatDiffM(path.properties.noises_diff[70], true)} m />
+            <DbLenPair dB={75} value={utils.formatDiffM(path.properties.noises_diff[75], true)} m />
           </FlexCols>
         </StyledPathStats>
       )
