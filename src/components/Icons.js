8a3d90a2689e813c35452c1743bfc6ace6c75cdd
Joose Helle <joose.helle@gmail.com>
2019-08-14 20:04:12 +0300

    Refactor icons

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,97 +1,61 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FaStar } from 'react-icons/fa'
 import { FiFilter } from 'react-icons/fi'
 import { IoIosArrowUp } from 'react-icons/io'
 import { IoIosArrowDown } from 'react-icons/io'
 import { IoIosArrowForward } from 'react-icons/io'
 import { IoIosArrowBack } from 'react-icons/io'
+import { IoIosClose } from 'react-icons/io'
+
+const iconStyle = `
+  vertical-align: middle;
+  display: table-cell;
+  text-align: center;
+`
+const Filter = styled(FiFilter)`
+  ${iconStyle}
+  font-size: 32px;
+`
+const ArrowUp = styled(IoIosArrowUp)`
+  ${iconStyle}
+  font-size: 43px;
+`
+const ArrowDown = styled(IoIosArrowDown)`
+  ${iconStyle}
+  font-size: 43px;
+`
+const ArrowForward = styled(IoIosArrowForward)`
+  ${iconStyle}
+  font-size: 31px;
+`
+const ArrowBack = styled(IoIosArrowBack)`
+  ${iconStyle}
+  font-size: 31px;
+`
+const Close = styled(IoIosClose)`
+  ${iconStyle}
+  font-size:  ${props => props.size || '31'}px;
+`
 
 const IconButton = styled.div`
   cursor: pointer;
   pointer-events: auto;
   padding: ${props => props.padding || '0px'};
-`
-export const IconDiv = styled.div`
   margin-left: ${props => props.leftMargin || '0px'};
   display: table;
   color: black;
   transition-duration: 0.2s;
   -webkit-transition-duration: 0.2s; /* Safari */
 `
-export const Star = styled(FaStar)`
-  font-size: 13px;
-  vertical-align: middle;
-  display: table-cell;
-  text-align: center;
-`
-const Filter = styled(FiFilter)`
-  font-size: 32px;
-  vertical-align: middle;
-  display: table-cell;
-  text-align: center;
-`
-const ArrowUp = styled(IoIosArrowUp)`
-  font-size: 43px;
-  vertical-align: middle;
-  display: table-cell;
-  text-align: center;
-`
-const ArrowDown = styled(IoIosArrowDown)`
-  font-size: 43px;
-  vertical-align: middle;
-  display: table-cell;
-  text-align: center;
-`
-const ArrowForward = styled(IoIosArrowForward)`
-  font-size: 31px;
-  vertical-align: middle;
-  display: table-cell;
-  text-align: center;
-`
-const ArrowBack = styled(IoIosArrowBack)`
-  font-size: 31px;
-  vertical-align: middle;
-  display: table-cell;
-  text-align: center;
-`
 
-export const FilterButton = ({ onClick }) => {
-  return (
-    <IconButton padding='5px' onClick={onClick}>
-      <IconDiv> <Filter /></IconDiv>
-    </IconButton>
-  )
-}
+export const FilterButton = ({ onClick }) => <IconButton padding='5px' onClick={onClick}> <Filter /> </IconButton>
 
-export const ArrowUpButton = ({ onClick }) => {
-  return (
-    <IconButton onClick={onClick}>
-      <IconDiv> <ArrowUp /></IconDiv>
-    </IconButton>
-  )
-}
+export const ArrowUpButton = ({ onClick }) => <IconButton onClick={onClick}> <ArrowUp /></IconButton>
 
-export const ArrowDownButton = ({ onClick }) => {
-  return (
-    <IconButton onClick={onClick}>
-      <IconDiv> <ArrowDown /></IconDiv>
-    </IconButton>
-  )
-}
+export const ArrowDownButton = ({ onClick }) => <IconButton onClick={onClick}> <ArrowDown /></IconButton>
 
-export const ArrowForwardButton = ({ onClick }) => {
-  return (
-    <IconButton onClick={onClick}>
-      <IconDiv leftMargin={'-3px'}> <ArrowForward /></IconDiv>
-    </IconButton>
-  )
-}
+export const ArrowForwardButton = ({ onClick }) => <IconButton onClick={onClick} leftMargin={'-3px'}> <ArrowForward /> </IconButton>
 
-export const ArrowBackButton = ({ onClick }) => {
-  return (
-    <IconButton onClick={onClick}>
-      <IconDiv leftMargin={'-6px'}> <ArrowBack /></IconDiv>
-    </IconButton>
-  )
-}
+export const ArrowBackButton = ({ onClick }) => <IconButton onClick={onClick} leftMargin={'-6px'}> <ArrowBack /></IconButton>
+
+export const CloseButton = ({ onClick, size }) => <IconButton onClick={onClick}> <Close size={size} /></IconButton>
