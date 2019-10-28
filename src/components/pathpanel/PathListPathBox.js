ccf006b89de30134997e827f3e68605809e11986
Joose Helle <joose.helle@gmail.com>
2019-10-28 20:46:38 +0200

    adjust dB color legend

--- src/components/pathpanel/PathListPathBox.js
+++ src/components/pathpanel/PathListPathBox.js
@@ -75,7 +75,6 @@ const ShortestPathBox = ({ path, selected, handleClick }) => {
 }
 
 const QuietPathBox = ({ path, selected, handleClick }) => {
-  const mdB_diff = path.properties.mdB_diff
   return (
     <StyledPathListPathBox selected={selected} onClick={handleClick}>
       <PathNoisesBar noisePcts={path.properties.noise_pcts} />
@@ -90,7 +89,7 @@ const QuietPathBox = ({ path, selected, handleClick }) => {
           {Math.round(path.properties.nei_diff_rat) + ' % noise'}
         </div>
         <div>
-          {Math.abs(mdB_diff) < 1 ? mdB_diff : Math.round(mdB_diff)} dB<sub>mean</sub>
+          {Math.round(path.properties.mdB)} dB<sub>mean</sub>
         </div>
       </PathPropsRow>
     </StyledPathListPathBox>
