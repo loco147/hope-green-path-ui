3b2ba5bc4bff43aeda5da664ae616e940b7057f6
Joose Helle <joose.helle@gmail.com>
2019-12-05 19:14:26 +0200

    Add tooltips to find path buttons

--- src/components/FindPathsButtons.js
+++ src/components/FindPathsButtons.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
-import { Button } from './Button'
 import { getCleanPaths, getQuietPaths } from '../reducers/pathsReducer'
 import { utils } from '../utils'
 
@@ -10,6 +9,39 @@ const OuterFlex = styled.div`
   flex-direction: column;
   margin: 0 10px 13px 10px;
   align-items: center;
+  @media (min-width: 550px) {
+    flex-direction: row;
+    justify-content: center;
+  }
+`
+const Button = styled.div`
+  cursor: pointer;
+  padding: 6px 13px;
+  color: white;
+  border-radius: 8px;
+  margin: 5px 6px;
+  font-weight: 400;
+  font-size: 28px;
+  text-align: center;
+  width: max-content;
+  letter-spacing: 1px;
+  max-width: 90%;
+  overflow: auto;
+  height: auto;
+  pointer-events: auto;
+  transition-duration: 0.2s;
+  -webkit-transition-duration: 0.2s; /* Safari */
+  border: 1px solid white;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.22), 0 6px 20px 0 rgba(0,0,0,0.14);
+  background-color: #17af40e8;
+  color: white;
+  &:hover { 
+    background-color: #128a32e8;
+  }
+`
+const Tooltip = styled.div`
+  font-size: 15px;
+  color: rgba(255,255,255,0.9);
 `
 
 const FindPathsButtons = (props) => {
@@ -26,10 +58,15 @@ const FindPathsButtons = (props) => {
   return (
     <OuterFlex>
       {cleanPathsAvailable
-        ? <Button border green shadow onClick={() => getCleanPaths(originCoords, targetCoords, routingId)}> Find clean paths</Button>
+        ? <Button
+          onClick={() => getCleanPaths(originCoords, targetCoords, routingId)}> Find clean paths
+          <Tooltip>by real-time air quality</Tooltip>
+        </Button>
         : null
       }
-      <Button border green shadow onClick={() => getQuietPaths(originCoords, targetCoords, routingId)}> Find quiet paths</Button>
+      <Button onClick={() => getQuietPaths(originCoords, targetCoords, routingId)}> Find quiet paths
+        <Tooltip>by typical traffic noise</Tooltip>
+      </Button>
     </OuterFlex>
   )
 }
