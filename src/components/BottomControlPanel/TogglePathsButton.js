6a8d8572a191deb0817166e14eeee8876309e78e
Joose Helle <joose.helle@gmail.com>
2019-12-19 14:11:07 +0200

    Adjust toggle path types button

--- src/components/BottomControlPanel/TogglePathsButton.js
+++ src/components/BottomControlPanel/TogglePathsButton.js
@@ -6,10 +6,9 @@ import { pathTypes } from '../../constants'
 
 const Button = styled.div`
   cursor: pointer;
-  padding: 4px 9px;
-  color: black;
-  border-radius: 5px;
-  border: 2px solid black;
+  padding: 5px 10px;
+  color: white;
+  border-radius: 30px;
   margin: 0px;
   font-weight: 500;
   font-size: 14px;
@@ -22,19 +21,31 @@ const Button = styled.div`
   pointer-events: auto;
   transition-duration: 0.2s;
   -webkit-transition-duration: 0.2s; /* Safari */
-  background-color: white;
+  background-color: black;
   @media (min-width: 600px) {
     &:hover { 
-      background-color: black;
-      color: white;
+      margin-bottom: 3px;
     }
   }
   ${props => props.disabled === true && css`
     cursor: default;
     pointer-events: none;
-    border-color: grey;
     color: grey;
   `}
+`
+
+const StyledPathTypeLabel = styled.span`
+  color: green;
+  ${props => props.toggleToPathType === 'quiet' && css`
+    &:before {
+      content: 'quiet';
+      color: #6ff7ff;
+    }`}
+  ${props => props.toggleToPathType === 'clean' && css`
+    &:before {
+      content: 'clean';
+      color: #74ff74;
+    }`}
 `
 
 const odsMatch = (quietPathOd, cleanPathOd) => {
@@ -48,21 +59,11 @@ const odsMatch = (quietPathOd, cleanPathOd) => {
   return true
 }
 
-const getTogglePathsButtonText = ({ showingPathsType, quietPathData, cleanPathData }) => {
-  if (!showingPathsType) return ''
-  if (showingPathsType === pathTypes.clean) {
-    return odsMatch(quietPathData.od, cleanPathData.od) ? 'Show quiet paths' : 'Find quiet paths'
-  } else if (showingPathsType === pathTypes.quiet) {
-    return odsMatch(quietPathData.od, cleanPathData.od) ? 'Show clean paths' : 'Find clean paths'
-  }
-}
-
 const getPathToggleFunc = (props) => {
   const { showingPathsType, quietPathData, cleanPathData, routingId } = props
   const { getSetQuietPaths, getSetCleanPaths, setQuietPaths, setCleanPaths } = props
 
   if (odsMatch(quietPathData.od, cleanPathData.od) === true) {
-    console.log('toggle paths')
     return showingPathsType === pathTypes.quiet
       ? setCleanPaths(quietPathData.od[0], quietPathData.od[1], routingId, cleanPathData.data)
       : setQuietPaths(cleanPathData.od[0], cleanPathData.od[1], routingId, quietPathData.data)
@@ -74,11 +75,14 @@ const getPathToggleFunc = (props) => {
 }
 
 const TogglePathsButton = (props) => {
+  const { cleanPathsAvailable, showingPathsType, quietPathData, cleanPathData } = props
+  const actionType = odsMatch(quietPathData.od, cleanPathData.od) ? 'Show' : 'Find'
+  const toggleToPathType = showingPathsType === pathTypes.clean ? 'quiet' : 'clean'
 
   return (
-    <Button disabled={!props.cleanPathsAvailable && props.showingPathsType === pathTypes.quiet}
+    <Button disabled={!cleanPathsAvailable && showingPathsType === pathTypes.quiet}
       onClick={() => getPathToggleFunc(props)}>
-      {getTogglePathsButtonText(props)}
+      {actionType} <StyledPathTypeLabel toggleToPathType={toggleToPathType} /> paths
     </Button>
   )
 }
