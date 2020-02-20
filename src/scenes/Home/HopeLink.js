b2b307849795eefb3362268dd614363a42ebc47a
Joose Helle <joose.helle@gmail.com>
2020-02-20 15:00:58 +0200

    Replace hope link with logo

--- src/scenes/Home/HopeLink.js
+++ src/scenes/Home/HopeLink.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import HopeLogo from '../Images/Hope_black_url.png'
 
 const Container = styled.div`
   position: absolute;
-  bottom: 6px;
+  bottom: 15px;
   left: 0px;
   right: 0px;
   z-index: 2;
   display: flex;
   justify-content: center;
+  pointer-events: none;
 `
 const StyledHopeLink = styled.a`
   color: white;
   border-radius: 5px;
+  pointer-events: auto;
   padding: 2px 6px;
-  font-size: 18px;
-  font-weight: 500;
-  letter-spacing: 1px;
   cursor: pointer;
   text-decoration: none;
   text-shadow: 1px 1px 2px rgba(0,0,0,0.8);
@@ -30,9 +30,9 @@ const StyledHopeLink = styled.a`
 const HopeLink = () => {
   return (
     <Container>
-      <StyledHopeLink href='https://ilmanlaatu.eu/'
-        target='_blank' rel='noopener noreferrer'>
-        HOPE/UIA</StyledHopeLink>
+      <StyledHopeLink href='https://ilmanlaatu.eu/' target='_blank' rel='noopener noreferrer'>
+        <img src={HopeLogo} width="116" height="30" alt="HopeLogo" />
+      </StyledHopeLink>
     </Container>
   )
 }
