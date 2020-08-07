c8b7dbd6cf1d593e4875eeb17f57fc23a4b477e7
Joose Helle <joose.helle@gmail.com>
2020-08-07 12:13:34 +0300

    Add caching for latest routing requests

--- src/services/paths.tsx
+++ src/services/paths.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { analytics } from './../firebase/firebase'
 import { ExposureMode } from '../constants'
+import * as cache from './cache'
 
 let baseurl = process.env.REACT_APP_QP_URL ? process.env.REACT_APP_QP_URL : ''
 
@@ -29,27 +30,41 @@ const formCoordinateString = (originCoords: number[], destinationCoords: number[
 export const getQuietPaths = async (travelMode: TravelMode, originCoords: number[], destinationCoords: number[]): Promise<PathDataResponse> => {
   const coordString = formCoordinateString(originCoords, destinationCoords)
   const queryUrl = baseurl.concat('paths/', travelMode, '/', ExposureMode.QUIET, '/', coordString)
-  console.log('Querying quiet paths:', queryUrl)
-  const response = await axios.get(queryUrl)
-  if (response.data.error) {
-    analytics.logEvent('routing_error_quiet_paths')
-    throw response.data.error
+  const cached = cache.getFromCache(queryUrl)
+  if (cached) {
+    console.log('Found quiet paths from cache:', queryUrl)
+    return cached
+  } else {
+    console.log('Querying quiet paths from server:', queryUrl)
+    const response = await axios.get(queryUrl)
+    if (response.data.error) {
+      analytics.logEvent('routing_error_quiet_paths')
+      throw response.data.error
+    }
+    analytics.logEvent('routed_quiet_paths')
+    cache.setToCacheWithExpiry(queryUrl, response.data, 3600)
+    return response.data
   }
-  analytics.logEvent('routed_quiet_paths')
-  return response.data
 }
 
 export const getCleanPaths = async (travelMode: TravelMode, originCoords: number[], destinationCoords: number[]): Promise<PathDataResponse> => {
   const coordString = formCoordinateString(originCoords, destinationCoords)
   const queryUrl = baseurl.concat('paths/', travelMode, '/', ExposureMode.CLEAN, '/', coordString)
-  console.log('Querying quiet paths:', queryUrl)
-  const response = await axios.get(queryUrl)
-  if (response.data.error) {
-    analytics.logEvent('routing_error_clean_paths')
-    throw response.data.error
+  const cached = cache.getFromCache(queryUrl)
+  if (cached) {
+    console.log('Found clean paths from cache:', queryUrl)
+    return cached
+  } else {
+    console.log('Querying clean paths from server:', queryUrl)
+    const response = await axios.get(queryUrl)
+    if (response.data.error) {
+      analytics.logEvent('routing_error_clean_paths')
+      throw response.data.error
+    }
+    analytics.logEvent('routed_clean_paths')
+    cache.setToCacheWithExpiry(queryUrl, response.data, 900)
+    return response.data
   }
-  analytics.logEvent('routed_clean_paths')
-  return response.data
 }
 
 export const debugNearestEdgeAttrs = async (lngLat: LngLat): Promise<void> => {
